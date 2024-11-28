1ac7c2e6d864d313d7cdf297fdd06de110ab6c12
Adrian <5121135+adrianh-za@users.noreply.github.com>
2024-11-28 14:03:47 +0000

    optimisations and fixes (#1)

--- GenerateLargeCSV/GenerateData.cs
+++ GenerateLargeCSV/GenerateData.cs
@@ -1,4 +1,5 @@
-﻿using Bogus;
+﻿using System.Diagnostics;
+using Bogus;
 using LibLargeCSV;
 using Person = LibLargeCSV.Person;
 
@@ -12,49 +13,89 @@ public class GenerateData(string outputFile, int rowCount)
     /// <summary>
     /// Generates a CSV file with random data of type <see cref="Person"/>
     /// </summary>
-    public async Task<string> Execute(int fakeBatchSize = 5000)
+    public async Task<string> Execute(int fakeBatchSize = 10000)
     {
-        var idHash = new HashSet<long>();
-        var random = new Random();
+        var sw = new Stopwatch();
 
-        //Creates a new file for the data
+        //Generate the list of unique IDs upfront for the number of records specified and put into a queue for easy popping
+        sw.Start();
+        var uniqueIdsQueue = new Queue<long>(GetUniqueIds(rowCount));
+        sw.Stop();
+        Console.WriteLine($"Generated {rowCount} unique IDs in {sw.Elapsed}");
+
+        //Creates a new file for the data (will overwrite if already exists)
         using var csvWriter = new CsvWriter(outputFile);
 
         //Create a data row and append into file created
+        sw.Restart();
+        var batchRowCounter = 0;
         var rowCounter = 0;
         while (rowCounter < rowCount)
         {
-            //Generate records in batches as specified (or less if remaining records are less than batch size)
+            //Generate records in batches as specified (or less if remaining records are less than batch size) for performance gains
             var recordsToGenerate = Math.Min(fakeBatchSize, rowCount - rowCounter);
 
             //Generate fake person
             var fakePerson = new Faker<Person>("en_GB")
-                .RuleFor(p => p.ID, f => GetUniqueId(idHash, random))
+                .RuleFor(p => p.ID, f => uniqueIdsQueue.Dequeue())
                 .RuleFor(p => p.Name, f => f.Person.FullName)
                 .RuleFor(p => p.CountryOfBirth, f => f.Address.Country())
                 .RuleFor(p => p.DateOfBirth, f => f.Date.Past(100));
 
             // Generate a fake person and write to file
             var persons = fakePerson.Generate(recordsToGenerate);
-            foreach(var person in persons)
+
+            //Write the records to the file
+            foreach (var person in persons)
+            {
                 await csvWriter.WriteRecord(person, Person.ToLine);
+            }
+
             rowCounter += recordsToGenerate;
+            batchRowCounter += recordsToGenerate;
+
+            //Output progress every 100,000 rows
+            if (batchRowCounter >= 100_000)
+            {
+                Console.WriteLine($"Added {batchRowCounter} rows to file in {sw.Elapsed}.  Total rows: {rowCounter} of {rowCount}");
+                sw.Restart();
+                batchRowCounter = 0;
+            }
         }
 
+        Console.WriteLine($"All {rowCount} added to file.");
         return outputFile;
     }
 
-
     /// <summary>
-    /// Get a unique ID excluding previously generated IDs
+    /// Get a unique ID excluding previously generated IDs in the specified hashset.
+    /// The new ID will be added to the hashset.
     /// </summary>
     private static long GetUniqueId(HashSet<long> idHash, Random random)
     {
         long id;
         do
         {
-            id = random.NextInt64(9999999999999999);
+            id = random.NextInt64(long.MaxValue);
         } while (!idHash.Add(id));
         return id;
+    }
+
+    /// <summary>
+    /// Get a list of unique IDs.
+    /// </summary>
+    private static List<long> GetUniqueIds(int count)
+    {
+        var ids = new List<long>(count);
+        var idHash = new HashSet<long>();
+        var random = new Random();
+
+        while (idHash.Count < count)
+        {
+            var id = GetUniqueId(idHash, random);
+            ids.Add(id);
+        }
+
+        return ids;
     }
 }
--- ProcessLargeCSV/Program.cs
+++ ProcessLargeCSV/Program.cs
@@ -4,19 +4,32 @@ using ReadLargeCSV;
 const string path = @"D:\temp\large-person.csv";
 const string chunkPath = @"D:\temp\\chunks\\large-person-{0}.csv";
 const string mergePath = @"D:\temp\large-person-merged.csv";
-const int chunkSize = 200_000;
+const int chunkSize = 1_000_000;
 
 //Split file into chunks
 var sw = new Stopwatch();
 sw.Start();
-var chunkedFiles = await ProcessData.ChunkFile(path, chunkPath, chunkSize);
+
+// ** Does convert string <> Person, slower but more type safe (1m 49s)
+//var chunkedFiles = await ProcessData.ChunkFile(path, chunkPath, chunkSize);
+
+// ** Does not convert string <> Person, merely reads and writes the lines (0m 40s)
+var chunkedFiles = await ProcessData.ChunkFileRaw(path, chunkPath, chunkSize);
+
 sw.Stop();
 Console.WriteLine($"Chunked into {chunkedFiles.Count} files in {sw.Elapsed}");
 
+
 //Sort the chunked files individually
 sw.Restart();
+
+// ** Single threaded sort is slower (3m 30s)
 //await ProcessData.SortFiles(chunkedFiles);
-await ProcessData.SortFilesParallel(chunkedFiles, 3);
+//await ProcessData.SortFilesParallel(chunkedFiles, 1);
+
+// ** Multi-threaded is fastest with 2 threads (2m 19s)
+await ProcessData.SortFilesParallel(chunkedFiles, 2);   //Most performant
+
 sw.Stop();
 Console.WriteLine($"Sorted {chunkedFiles.Count} files in {sw.Elapsed}");
 
